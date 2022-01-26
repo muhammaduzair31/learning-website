9257cccfdbebac8e8086d9a218ad2a1efd8105a7
muhammad uzair <93971533+muhammaduzair31@users.noreply.github.com>
2022-01-26 22:45:08 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+/*===== SHOW NAVBAR  =====*/ 
+const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd){
+        toggle.addEventListener('click', ()=>{
+            // show navbar
+            nav.classList.toggle('show')
+            // change icon
+            toggle.classList.toggle('bx-x')
+            // add padding to body
+            bodypd.classList.toggle('body-pd')
+            // add padding to header
+            headerpd.classList.toggle('body-pd')
+        })
+    }
+}
+
+showNavbar('header-toggle','nav-bar','body-pd','header')
+
+/*===== LINK ACTIVE  =====*/ 
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(l=> l.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
+
+
+
+var width = 100,
+    perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
+    EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
+    time = parseInt((EstimatedTime/1000)%60)*100;
+
+// Loadbar Animation
+$(".loadbar").animate({
+  width: width + "%"
+}, time);
+
+// Loadbar Glow Animation
+$(".glow").animate({
+  width: width + "%"
+}, time);
+
+// Percentage Increment Animation
+var PercentageID = $("#precent"),
+		start = 0,
+		end = 100,
+		durataion = time;
+		animateValue(PercentageID, start, end, durataion);
+		
+function animateValue(id, start, end, duration) {
+  
+	var range = end - start,
+      current = start,
+      increment = end > start? 1 : -1,
+      stepTime = Math.abs(Math.floor(duration / range)),
+      obj = $(id);
+    
+	var timer = setInterval(function() {
+		current += increment;
+		$(obj).text(current + "%");
+      //obj.innerHTML = current;
+		if (current == end) {
+			clearInterval(timer);
+		}
+	}, stepTime);
+}
+
+// Fading Out Loadbar on Finised
+setTimeout(function(){
+  $('.preloader-wrap').fadeOut(300);
+}, time);
